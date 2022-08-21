e1e69c91335feb685ab4375d38f3a6d651c45afa
Robert Shepley <shepleysound@gmail.com>
2022-08-20 19:21:29 -0700

    Refactor Collider object constructor to class.

--- js/Collider.js
+++ js/Collider.js
@@ -1,34 +1,36 @@
-function Collider(element) {
-  this.element = element;
-  this.rect = element.getBoundingClientRect();
-  Collider.instances.push(this);
-
-}
-
-Collider.instances = [];
-
-Collider.prototype.deleteCollider = function() {
-  for (let i = 0; i < Collider.instances.length; i++) {
-    if (Collider.instances[i].element === this.element) {
-      Collider.instances.splice(i, 1);
-    }
+class Collider {
+  constructor(element) {
+    this.element = element;
+    this.rect = element.getBoundingClientRect();
+    Collider.instances.push(this);
   }
-};
 
-Collider.checkCollisions = function() {
-  const checkInstances = Collider.instances.slice();
-  while (checkInstances.length > 1) {
-    const colliderOne = checkInstances[0];
-    checkInstances.shift();
-    for (const colliderTwo of checkInstances) {
-      const c1 = colliderOne.element.getBoundingClientRect();
-      const c2 = colliderTwo.element.getBoundingClientRect();
-      if (c1.x < c2.x + c2.width &&
-        c1.x + c1.width > c2.x &&
-        c1.y < c2.y + c2.height &&
-        c1.height + c1.y > c2.y) {
-        return [colliderOne, colliderTwo];
+  deleteCollider() {
+    for (let i = 0; i < Collider.instances.length; i++) {
+      if (Collider.instances[i].element === this.element) {
+        Collider.instances.splice(i, 1);
       }
     }
   }
-};
+
+  static checkCollisions() {
+    const checkInstances = Collider.instances.slice();
+    while (checkInstances.length > 1) {
+      const colliderOne = checkInstances[0];
+      checkInstances.shift();
+      for (const colliderTwo of checkInstances) {
+        const c1 = colliderOne.element.getBoundingClientRect();
+        const c2 = colliderTwo.element.getBoundingClientRect();
+        if (c1.x < c2.x + c2.width &&
+          c1.x + c1.width > c2.x &&
+          c1.y < c2.y + c2.height &&
+          c1.height + c1.y > c2.y) {
+          return [colliderOne, colliderTwo];
+        }
+      }
+    }
+  }
+
+  static instances = [];
+
+}
